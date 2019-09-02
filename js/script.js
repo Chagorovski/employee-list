7fd4ae0bce95dc1727c0ac32c47b04c333f2437c
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-09-02 11:45:37 +0200

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -67,35 +67,3 @@ const appendPageLinks = (list) => {
 };
 
 appendPageLinks(ulListChildredn);
-
-const pageHeader = document.querySelector('.page-header');
-const searchHeaderDiv = document.createElement('div');
-const search = document.createElement('input');
-const button = document.createElement('button');
-
-searchHeaderDiv.className = 'student-search';
-search.placeholder = 'Student Search';
-button.textContent = 'Search';
-button.id = 'search-button';
-
-pageHeader.appendChild(searchHeaderDiv);
-searchHeaderDiv.appendChild(search);
-searchHeaderDiv.appendChild(button);
-
-function myFunction() {
-   var input, filter, ul, li, a, i, txtValue;
-   input = document.getElementById("myInput");
-   filter = input.value.toUpperCase();
-   ul = document.getElementById("myUL");
-   li = ul.getElementsByTagName("li");
-   for (i = 0; i < li.length; i++) {
-       a = li[i].getElementsByTagName("a")[0];
-       txtValue = a.textContent || a.innerText;
-       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-           li[i].style.display = "";
-       } else {
-           li[i].style.display = "none";
-       }
-   }
-} 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
