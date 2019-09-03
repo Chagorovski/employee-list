311e36c11dfc133859752bf9fa7aa54ed03e9420
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-09-03 14:01:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,32 +38,72 @@ const appendPageLinks = (list) => {
    const ul = document.createElement('ul');
    const li = ul.children;
    paginationDiv.appendChild(ul);
-
    for (let i = 0; i < list.length / itemsPerPage; i ++) {
       const li = document.createElement('li');
       const a = document.createElement('a');
-
       if (i === 0) {
          a.className = 'active';
       }
-   
       ul.appendChild(li);
       li.appendChild(a);
       a.href = '#';
       a.textContent = i + 1;
       aNumText = a.textContent; 
    } 
-
    ul.addEventListener('click', (e) => {
       const pageNumber = e.target.textContent;
       for (let i = 0; i < ul.children.length; i++) {
-         const a = li[i].firstElementChild;
+         const a = li[i].firstElementChild;  
          if (a.className = true) {
             a.className = null;
          }      
       }
+      showPage(ulListChildredn,pageNumber);
       e.target.className = 'active';
-   });
+   }); 
 };
 
 appendPageLinks(ulListChildredn);
+
+// Function to add the search bar with submit button and the event listeners for them
+
+function addSearchBar () {
+   const pageHeader = document.querySelector('.page-header');
+   const searchHeaderDiv = document.createElement('div');
+   const search = document.createElement('input');
+   const button = document.createElement('button');
+
+   searchHeaderDiv.className = 'student-search';
+   search.placeholder = 'Student Search';
+   button.textContent = 'Search';
+ 
+   pageHeader.appendChild(searchHeaderDiv);
+   searchHeaderDiv.appendChild(search);
+   searchHeaderDiv.appendChild(button);
+
+   button.addEventListener ('click' , () => {
+      event.preventDefault();
+      makeSearch(search,ulListChildredn);
+   });
+   search.addEventListener('keyup', () => {
+      makeSearch(search,ulListChildredn);
+   });
+ };
+
+// Function for showing the search results using includes() method
+
+function makeSearch(search,students) {
+   const filter = search.value.toLowerCase();
+   const arr = [];
+   for (let i = 0; i < students.length; i++) {
+      const listItemName = students[i].querySelector('h3').textContent;
+      students[i].style.display = 'none';
+      if (filter.length !==0 && listItemName.toLowerCase().includes(filter)) {
+         students[i].style.display = '';
+         arr.push(students[i]);
+      }
+   }
+};
+
+
+addSearchBar();
